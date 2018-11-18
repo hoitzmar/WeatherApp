ba600902562b2fe0720d9bae79ddf08a6521414f
hoitzmar <44920308+hoitzmar@users.noreply.github.com>
2018-11-18 11:36:06 +0100

    Simple Weather's Weather Model Object

---
+++ simple-weather/src/main/java/de/fh/albsig/hoitzmar/weather/Weather.java
@@ -0,0 +1,71 @@
+package src.main.java.de.fh.albsig.hoitzmar.weather;
+
+public class Weather {
+    private String city;
+    private String region;
+    private String country;
+    private String condition;
+    private String temp;
+    private String chill;
+    private String humidity;
+
+    public Weather() {
+    }
+
+    public String getChill() {
+        return chill;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getCondition() {
+        return condition;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getHumidity() {
+        return humidity;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getTemp() {
+        return temp;
+    }
+
+    public void setChill(final String chill) {
+        this.chill = chill;
+    }
+
+    public void setCity(final String city) {
+        this.city = city;
+    }
+
+    public void setCondition(final String condition) {
+        this.condition = condition;
+    }
+
+    public void setCountry(final String country) {
+        this.country = country;
+    }
+
+    public void setHumidity(final String humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setRegion(final String region) {
+        this.region = region;
+    }
+
+    public void setTemp(final String temp) {
+        this.temp = temp;
+    }
+
+}
